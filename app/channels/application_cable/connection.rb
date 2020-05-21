17325c06a39c2229ac60631081c2ebfdaec83eb2
José Sousa <josesousa9000@gmail.com>
2020-05-21 22:06:41 +0100

    Authenticate action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      verified_user = User.find_by(id: request.session["user_id"])
+
+      verified_user || reject_unauthorized_connection
+    end
   end
 end
