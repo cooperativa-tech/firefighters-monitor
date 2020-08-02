d2e92699531f25df83ddf1d055fff84060c57fd7
José Sousa <josesousa9000@gmail.com>
2020-08-02 19:04:34 +0100

    Support IE11

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,3 +1,10 @@
+import "@stimulus/polyfills";
+
+// Stimulus Reflex workaround for IE11
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
