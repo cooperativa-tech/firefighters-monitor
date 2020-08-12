1cdcd5c43dd54fb0e2f175ca077977f3ed467ad9
Francisco Sousa <josesousa9000@gmail.com>
2020-08-12 11:04:00 +0100

    Generate random password on create on the admin dashboard (#13)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,32 @@
 module Admin
   class UsersController < Admin::ApplicationController
+    def create
+      user = create_user
+
+      if user.save
+        redirect_to(
+          [namespace, user],
+          notice: translate_with_resource("create.success"),
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, user)
+        }
+      end
+    end
+
+    private
+
+    def create_user
+      user = resource_class.new(resource_params)
+      authorize_resource(user)
+
+      unless user.password
+        user.password = SecureRandom.hex
+        user.password_confirmation = user.password
+      end
+
+      user
+    end
   end
 end
