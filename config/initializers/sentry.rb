2dd4524208499ca6f2b76197a466379198b6697a
Francisco Sousa <francisco.sousa@hey.com>
2020-10-26 16:21:03 +0000

    Hide sentry variable and disable by default

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
 Raven.configure do |config|
-  config.dsn = "https://cb3f6cba3bf742cd8b1b6bbf88749053@o456594.ingest.sentry.io/5449816"
+  config.dsn = ENV["SENTRY_DSN"]
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
