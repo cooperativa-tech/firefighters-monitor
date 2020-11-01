e76ea421e290e4b78a663b534152a5a0ad4b39fe
José Sousa <josesousa9000@gmail.com>
2020-11-01 18:39:25 +0000

    Fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,6 +3,6 @@ Rails.application.config.cache_store = :redis_cache_store, {
   url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }
 }
 Rails.application.config.session_store :active_record_store,
-  serializer: :json,
-  key: "_firefighters_monitor_session",
-  same_site: :lax
+                                       serializer: :json,
+                                       key: "_firefighters_monitor_session",
+                                       same_site: :lax
