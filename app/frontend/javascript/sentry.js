5ce2e4265071b1adebe6fa98412541710fe33576
Francisco Sousa <francisco.sousa@hey.com>
2021-01-11 16:24:28 +0000

    HOTFIX: Just log sentry errors in prod

--- app/frontend/javascript/sentry.js
+++ app/frontend/javascript/sentry.js
@@ -1,15 +1,15 @@
 import * as Sentry from "@sentry/browser";
 import { Integrations } from "@sentry/tracing";
 
-Sentry.init({
-  dsn:
-    "https://ed4dcca3666945358d7793212359fcc5@o456594.ingest.sentry.io/5449846",
-  integrations: [new Integrations.BrowserTracing()],
-  tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV || "development",
-});
-
 if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn:
+      "https://ed4dcca3666945358d7793212359fcc5@o456594.ingest.sentry.io/5449846",
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+    environment: "production",
+  });
+
   window.onerror = (_message, _source, _lineno, _colno, _error) => {
     window.location = "500.html";
   };
