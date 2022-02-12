9eec56341dd6e81142288ce3779f3784836cad52
Francisco Sousa <francisco.sousa@hey.com>
2022-02-12 17:43:52 +0000

    Update versions

--- app/frontend/javascript/sentry.js
+++ app/frontend/javascript/sentry.js
@@ -3,8 +3,7 @@ import { Integrations } from "@sentry/tracing";
 
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
-    dsn:
-      "https://ed4dcca3666945358d7793212359fcc5@o456594.ingest.sentry.io/5449846",
+    dsn: "https://ed4dcca3666945358d7793212359fcc5@o456594.ingest.sentry.io/5449846",
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 1.0,
     environment: "production",
